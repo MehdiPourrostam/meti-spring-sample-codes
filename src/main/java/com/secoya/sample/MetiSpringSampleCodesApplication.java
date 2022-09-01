049e24d0b913a5c79af0255aa1b7bfb20460586c
Mehdi Pourrostam <mehdi.pourrostam@gmail.com>
2022-09-01 19:42:36 +0430

    first commit

---
+++ src/main/java/com/secoya/sample/MetiSpringSampleCodesApplication.java
@@ -0,0 +1,13 @@
+package com.secoya.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetiSpringSampleCodesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MetiSpringSampleCodesApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/secoya/sample/MetiSpringSampleCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secoya.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetiSpringSampleCodesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
